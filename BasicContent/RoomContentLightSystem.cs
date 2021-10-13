e6644b72b585bc3296a42384134de4cc79641a9b
Maxime Charbonneau <max@parabole.ca>
2021-10-13 16:58:58 -0400

    Null check int renderer/light system

--- BasicContent/RoomContentLightSystem.cs
+++ BasicContent/RoomContentLightSystem.cs
@@ -1,6 +1,7 @@
 using Parabole.RoomSystem.Core.Room;
 using Parabole.RoomSystem.Core.Room.Components;
 using Unity.Entities;
+using UnityEngine;
 
 namespace Parabole.RoomSystem.BasicContent
 {
@@ -9,23 +10,44 @@ namespace Parabole.RoomSystem.BasicContent
 	{
 		protected override void OnUpdate()
 		{
-			Entities.WithoutBurst().WithAll<JustVisibleRoom>().ForEach((RoomContentLight content) =>
+			Entities.WithoutBurst().WithAll<JustVisibleRoom>().ForEach((Entity entity, RoomContentLight content) =>
 			{
 				var lights = content.Lights;
 				for (int i = 0; i < lights.Length; i++)
 				{
-					lights[i].enabled = true;
+					SetLight(entity, lights[i], true);
 				}
 			}).Run();
 			
-			Entities.WithoutBurst().WithAll<JustNotVisibleRoom>().ForEach((RoomContentLight content) =>
+			Entities.WithoutBurst().WithAll<JustNotVisibleRoom>().ForEach((Entity entity, RoomContentLight content) =>
 			{
 				var lights = content.Lights;
 				for (int i = 0; i < lights.Length; i++)
 				{
-					lights[i].enabled = false;
+					SetLight(entity, lights[i], false);
 				}
 			}).Run();
+		}
+
+		private void SetLight(Entity entity, Light light, bool value)
+		{
+			if (light == null)
+			{
+				var name = GetName(EntityManager, entity);
+				Debug.LogError($"Light on room content has been destroyed on entity {name}");
+				return;
+			}
+
+			light.enabled = value;
+		}
+		
+		private static string GetName(EntityManager manager, Entity entity)
+		{
+#if UNITY_EDITOR
+			return manager.GetName(entity);
+#else
+			return entity.ToString();
+#endif
 		}
 	}
 }
