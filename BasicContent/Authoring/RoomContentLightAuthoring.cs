1460b3a165233c7bab9e8575ed011d83b392e472
Maxime Charbonneau <max@parabole.ca>
2021-10-13 15:19:58 -0400

    Better error handling

--- BasicContent/Authoring/RoomContentLightAuthoring.cs
+++ BasicContent/Authoring/RoomContentLightAuthoring.cs
@@ -1,23 +1,40 @@
+using System.Collections.Generic;
 using System.Linq;
 using Unity.Entities;
 using UnityEngine;
 
 namespace Parabole.RoomSystem.BasicContent.Authoring
 {
-	[RequiresEntityConversion]
 	public class RoomContentLightAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 	{
 		[SerializeField] private GameObject[] lightParents = null;
 		
 		public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
 		{
-			var lights = lightParents.SelectMany(x => x.GetComponentsInChildren<Light>()).Distinct().ToArray();
+			var lightList = new List<Light>();
+			for (int i = 0; i < lightParents.Length; i++)
+			{
+				var parent = lightParents[i];
+				if (parent == null)
+				{
+					Debug.LogError($"Unassigned light parent in {gameObject.name}", gameObject);
+					continue;
+				}
+				var currentRenderers = lightParents[i].GetComponentsInChildren<Light>();
+				lightList.AddRange(currentRenderers);
+			}
 
+			AssignLightArray(entity, dstManager, lightList);
+		}
+
+		private static void AssignLightArray(Entity entity, EntityManager dstManager, List<Light> lightList)
+		{
+			var lights = lightList.Distinct().ToArray();
 			for (int i = 0; i < lights.Length; i++)
 			{
 				lights[i].enabled = false;
 			}
-			
+
 			var lightContent = new RoomContentLight
 			{
 				Lights = lights,
--- BasicContent/Authoring/RoomContentRendererAuthoring.cs
+++ BasicContent/Authoring/RoomContentRendererAuthoring.cs
@@ -1,29 +1,45 @@
+using System.Collections.Generic;
 using System.Linq;
 using Unity.Entities;
 using UnityEngine;
 
 namespace Parabole.RoomSystem.BasicContent.Authoring
 {
-	[RequiresEntityConversion]
 	public class RoomContentRendererAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 	{
 		[SerializeField] private GameObject[] rendererParents = null;
 		
 		public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
 		{
-			var renderers = rendererParents.SelectMany(x => x.GetComponentsInChildren<Renderer>()).Distinct().ToArray();
+			var rendererList = new List<Renderer>();
+			for (int i = 0; i < rendererParents.Length; i++)
+			{
+				var parent = rendererParents[i];
+				if (parent == null)
+				{
+					Debug.LogError($"Unassigned renderer parent in {gameObject.name}", gameObject);
+					continue;
+				}
+				var currentRenderers = rendererParents[i].GetComponentsInChildren<Renderer>();
+				rendererList.AddRange(currentRenderers);
+			}
+			
+			AssignArray(entity, dstManager, rendererList);
+		}
+
+		private static void AssignArray(Entity entity, EntityManager dstManager, List<Renderer> rendererList)
+		{
+			var renderers = rendererList.Distinct().ToArray();
 
 			for (int i = 0; i < renderers.Length; i++)
 			{
 				renderers[i].enabled = false;
 			}
-			
-			var rendererContent = new RoomContentRenderer
+
+			dstManager.AddComponentData(entity, new RoomContentRenderer
 			{
 				Renderers = renderers,
-			};
-
-			dstManager.AddComponentData(entity, rendererContent);
+			});
 		}
 	}
 }
